608754bf10d9677323c0b79b9dfb2fc4bf92fa74
Kellie Ryan <littlekel03@gmail.com>
2021-06-05 16:15:58 +0800

    Created Databse and added Models

---
+++ Develop/models/User.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class User extends Model {}
+
+User.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+
+      username: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+
+      email: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        validate: {
+          len:[8],
+        }
+      },
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'user',
+    }
+  );
+  
+  module.exports = User;
+  
